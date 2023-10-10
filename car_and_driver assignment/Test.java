898bd0c43f33adeb1086e2913b39e33e9dd4a2aa
Armando Robles <145632729+Armand1218@users.noreply.github.com>
2023-10-09 19:43:08 -0700

    Add files via upload

---
+++ car_and_driver assignment/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    public static void main(String[] args) {
+        Driver driver = new Driver();
+
+        //
+        driver.drive();
+        driver.drive();
+        driver.drive();
+        driver.drive();
+
+        driver.boost();
+
+        driver.refuel();
+        driver.refuel();
+        driver.refuel();
+    }
+}
